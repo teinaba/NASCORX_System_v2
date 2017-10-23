6b136e5d35ed8bb16ec64adb604c32cf70658d37
tetsuinaba <inaba.t@a.phys.nagoya-u.ac.jp>
2017-10-23 17:27:30 +0900

    add measure method.

--- device/L218.py
+++ device/L218.py
@@ -3,14 +3,13 @@
 
 
 #import modules
-import time, sys
 import pymeasure
 
 class l218(object):
-    '''
+    """
     DESCRIPTION
     ================
-    This class cntrols the Lakeshore model218.
+    This class controls the Lakeshore model218.
 
     ARGUMENTS
     ================
@@ -21,37 +20,60 @@ class l218(object):
         Number: 0-30
         Type: string
         Default: 1
-    '''
+    """
 
     def __init__(self, IP='192.168.101.78', GPIB=1):
         self.IP = IP
         self.GPIB = GPIB
         self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
-        
-    def measure(self, ch=1):
-        '''        
+
+    def measure(self):
+        """
         DESCRIPTION
         ================
-
         This function queries the temperature.
-        
+
+        ARGUMENTS
+        ================
+        Nothing.
+
+        RETURNS
+        ================
+        1. The temperature [K]
+             Type   : float list
+             Length : 8
+        """
+        self.com.open()
+        self.com.send('KRDG?')
+        raw = self.com.readline()
+        ret = raw.strip().split(',')
+        temp = list(map(float, ret))
+        self.com.close()
+        return temp
+
+    def measure_ch(self, ch=1):
+        """
+        DESCRIPTION
+        ================
+        This function queries the temperature.
+
         ARGUMENTS
         ================
         1. ch: a sensor channel
-            Number: 1-8 
+            Number: 1-8
             Type: int
             Default: 1
-        
+
         RETURNS
         ================
-        1. The temperature [K]: float type
-        '''
-        if 1<=ch<=8:
+        1. The temperature [K]: float
+        """
+        if 1 <= ch <= 8:
             self.com.open()
             self.com.send('KRDG? %d'%(ch))
             ret = self.com.readline()
             self.com.close()
-	    print(ret)
+            print(ret)
             temp = float(ret)
         else:
             print('***********************')
@@ -63,3 +85,4 @@ class l218(object):
 
 #written by K.Urushihara
 # 2017/08/29 T.Inaba: delete sys.path to pymeasure
+# 2017/10/23 T.Inaba: add measure method.
