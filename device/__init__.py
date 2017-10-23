67045efdb850099dcc3ef4b44ac8e29f5b59bdb2
tetsuinaba <inaba.t@a.phys.nagoya-u.ac.jp>
2017-10-23 14:05:19 +0900

    deleted comment-out of TR7xW

---
+++ device/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+import NASCORX_System.device.A11713A
+import NASCORX_System.device.A11713C
+import NASCORX_System.device.CPZ3177
+import NASCORX_System.device.CPZ340516
+import NASCORX_System.device.CPZ340816
+import NASCORX_System.device.CPZ7204
+import NASCORX_System.device.E8257D
+import NASCORX_System.device.FSW_0020
+import NASCORX_System.device.L218
+import NASCORX_System.device.MG3692C
+import NASCORX_System.device.ML2437A
+import NASCORX_System.device.TR71W
+import NASCORX_System.device.TR72W
