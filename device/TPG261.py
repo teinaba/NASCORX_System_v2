a92f52b6eb9f9af277853e4cdf016d0ad0284ae4
tetsuinaba <inaba.t@a.phys.nagoya-u.ac.jp>
2017-10-23 15:20:29 +0900

    adapted to Python 3

--- device/TPG261.py
+++ device/TPG261.py
@@ -2,13 +2,15 @@
 # _*_ coding: UTF-8 _*_
 
 #import modules
-import socket, time, csv
+import time
+import socket
+
 
 class tpg261(object):
-    '''
+    """
     DESCRIPTION
     ================
-    This class cntrols the TPG261.
+    This class controls the TPG261.
     ////TPG261 Data Format////
     start bit: 1
     data bit: 8
@@ -49,7 +51,7 @@ class tpg261(object):
             4) Ping reply timer - 10 sec
             5) Ping maximum retries for disconnect - 1
         4) Client connection
-        5) Timer 
+        5) Timer
         6) DTR/RTS signal
         7) Ethernet link monitor
 
@@ -61,15 +63,15 @@ class tpg261(object):
     2. port: port number of the FA-210
         Type: int
         Default: 9600
-    '''
-    def __init__(self, IP = '192.168.100.1', port = 9600):
+    """
+    def __init__(self, IP='192.168.100.1', port=9600):
         self.IP = IP
         self.port = port
         self.clientsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.clientsock.connect((self.IP, self.port))
 
     def query_pressure(self):
-        """        
+        """
         DESCRIPTION
         ================
         This function queries the pressure value.
@@ -83,18 +85,19 @@ class tpg261(object):
         1. pressure: pressure value of the TPG261
             Type: float [Bar, Torr or Pascal]
         """
-        self.clientsock.sendall('PR1 \r\n')
+        self.clientsock.sendall(bytes('PR1 \r\n', 'utf8'))
         time.sleep(0.5)
-        self.clientsock.sendall('\x05')
+        self.clientsock.sendall(bytes('\x05', 'utf8'))
         time.sleep(0.5)
         raw = self.clientsock.recv(256)
-        line = raw.split('\r\n')
-        rets = line[1].split(',')
-        pressure = float(rets[1])
+        raw_dec = raw.decode('utf8')
+        line = raw_dec.split('\r\n')
+        ret = line[1].split(',')
+        pressure = float(ret[1])
         return pressure
 
     def query_unit(self):
-        """        
+        """
         DESCRIPTION
         ================
         This function queries the unit of pressure.
@@ -109,22 +112,23 @@ class tpg261(object):
             Type: string ('bar', 'torr', or 'pascal')
         """
         parameter = ['bar', 'torr', 'pascal']
-        self.clientsock.sendall('UNI \r\n')
+        self.clientsock.sendall(bytes('UNI \r\n', 'utf8'))
         time.sleep(0.5)
-        self.clientsock.sendall('\x05')
+        self.clientsock.sendall(bytes('\x05', 'utf8'))
         time.sleep(0.5)
         raw = self.clientsock.recv(256)
-        line = raw.split('\r\n')
-        rets = line[1].split(',')
-        index = int(rets[0])
-        if index==0 or 1 or 2:
+        raw_dec = raw.decode('utf8')
+        line = raw_dec.split('\r\n')
+        ret = line[1].split(',')
+        index = int(ret[0])
+        if index == 0 or 1 or 2:
             unit = parameter[index]
         else:
             unit = 'UNKNOWN'
         return unit
 
     def set_unit(self, unit='torr'):
-        """        
+        """
         DESCRIPTION
         ================
         This function sets the unit of pressure.
@@ -140,21 +144,22 @@ class tpg261(object):
         ================
         Nothing.
         """
-        if unit=='bar':
-            self.clientsock.sendall('UNI,0 \r\n')
-        elif unit=='torr':
-            self.clientsock.sendall('UNI,1 \r\n')
-        elif unit=='pascal':
-            self.clientsock.sendall('UNI,2 \r\n')
+        if unit == 'bar':
+            self.clientsock.sendall(bytes('UNI,0 \r\n', 'utf8'))
+        elif unit == 'torr':
+            self.clientsock.sendall(bytes('UNI,1 \r\n', 'utf8'))
+        elif unit == 'pascal':
+            self.clientsock.sendall(bytes('UNI,2 \r\n', 'utf8'))
         else:
             print('!!!!ERROR!!!!')
             print('invalid unit: '+str(unit))
             print('available unit: "bar", "torr" or "pascal"')
             quit()
         time.sleep(0.5)
-        self.clientsock.sendall('\x05')
+        self.clientsock.sendall(bytes('\x05', 'utf8'))
         time.sleep(0.5)
         raw = self.clientsock.recv(256)
+        raw_dec = raw.decode('utf8')
         return 
 
 
@@ -171,25 +176,26 @@ class tpg261(object):
         RETURNS
         ================
         1. bau: bau rate of the TPG261
-            Type: int (9600, 19200 or 38400)
+            Type: int (9600, 19200 or 38400) or 0 (IF index is INVALID)
         """
         parameter = [9600, 19200, 38400]
-        self.clientsock.sendall('BAU \r\n')
+        self.clientsock.sendall(bytes('BAU \r\n', 'utf8'))
         time.sleep(0.5)
-        self.clientsock.sendall('\x05')
+        self.clientsock.sendall(bytes('\x05', 'utf8'))
         time.sleep(0.5)
         raw = self.clientsock.recv(256)
-        line = raw.split('\r\n')
-        rets = line[1].split(',')
-        index = int(rets[0])
-        if index==0 or 1 or 2:
+        raw_dec = raw.decode('utf8')
+        line = raw_dec.split('\r\n')
+        ret = line[1].split(',')
+        index = int(ret[0])
+        if index == 0 or 1 or 2:
             bau = int(parameter[index])
         else:
-            pass
+            bau = 0
         return bau
 
     def set_baurate(self, rate=9600):
-        """        
+        """
         DESCRIPTION
         ================
         This function sets the bau rate of the TPG261.
@@ -205,26 +211,27 @@ class tpg261(object):
         ================
         Nothing.
         """
-        if rate==9600:
-            self.clientsock.sendall('BAU,0 \r\n')
-        elif rate==19200:
-            self.clientsock.sendall('BAU,1 \r\n')
-        elif rate==38400:
-            self.clientsock.sendall('BAU,2 \r\n')
+        if rate == 9600:
+            self.clientsock.sendall(bytes('BAU,0 \r\n', 'utf8'))
+        elif rate == 19200:
+            self.clientsock.sendall(bytes('BAU,1 \r\n', 'utf8'))
+        elif rate == 38400:
+            self.clientsock.sendall(bytes('BAU,2 \r\n', 'utf8'))
         else:
             print('!!!!ERROR!!!!')
             print('invalid bau rate: '+str(rate))
             print('available bau rate: 9600, 19200 or 38400')
             quit()
         time.sleep(0.5)
-        self.clientsock.sendall('\x05')
+        self.clientsock.sendall(bytes('\x05', 'utf8'))
         time.sleep(0.5)
         raw = self.clientsock.recv(256)
+        raw_dec = raw.decode('utf8')
         return
 
 
     def query_gauge(self):
-        """        
+        """
         DESCRIPTION
         ================
         This function queries the pressure value.
@@ -238,24 +245,27 @@ class tpg261(object):
         1. sensor: sensor type connected to the TPG261
             Type: list ([0]: ch1, [1]: ch2)
         """
-        self.clientsock.sendall("TID\r\n")
+        self.clientsock.sendall(bytes('TID\r\n', 'utf8'))
         time.sleep(0.5)
-        self.clientsock.sendall("\x05")
+        self.clientsock.sendall(bytes('\x05', 'utf8'))
         time.sleep(0.5)
         raw = self.clientsock.recv(1024)
-        line = raw.split('\r\n')
+        raw_dec = raw.decode('utf8')
+        line = raw_dec.split('\r\n')
         ret = line[1].split(',')
         return ret
 
 
 '''
-Last Update 2016/01/15
+Last Update 2017/10/23
 
 ***HISTORY***
 *2015/11/26 K.Urushihara
-ファイル作成
+create file
 *2016/01/15 K.Urushihara
 add measure()
 add gauge_query()
+*2017/10/23 T.Inaba
+adapt to Python 3
 *************
 '''
